c32bf396c03602d5926cff66f502b67ca11cb9a6
Mr.William <141992777+wetoon@users.noreply.github.com>
2025-06-10 09:31:13 +0000

    new style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
     return (
         <>
-            <div className='pt-5 w-60 mx-auto'>
+            <div className='pt-5 w-80 mx-auto max-w-full'>
                 <video className='w-full' src='/stream/video?watch=14HeZt5EQ2dxdBCNfa_kWHqesUEb0cKPK' controls={ true }></video>
             </div>
         </>
