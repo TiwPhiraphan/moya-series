a5f9b085a46019310047fa8da5ff52857c32e7b9
Mr.William <141992777+wetoon@users.noreply.github.com>
2025-06-10 08:04:35 +0000

    First change

--- src/app/stream/video/route.ts
+++ src/app/stream/video/route.ts
@@ -1,7 +1,19 @@
 
 import { type NextRequest, NextResponse } from 'next/server'
 import { auth } from '@package/module/GoogleAuth'
-import { redirect } from 'next/navigation'
+
+type GoogleApisResponse = {
+    error: { code: number, message: string }
+    mimeType: string
+    size: string
+}
+
+function getParsedInt( num: unknown ) {
+    return Number.isInteger( num ) ? parseInt( num as string ) : false
+}
+
+const CHUNK_SIZE = 1048576
+const videoMetadata = new Map< string, Omit< GoogleApisResponse, 'error' > >()
 
 export async function GET( request: NextRequest ) {
 
@@ -10,8 +22,47 @@ export async function GET( request: NextRequest ) {
     const id = searchParams.get('watch')
 
     if ( typeof id !== 'string' || typeof range !== 'string' ) {
-        return redirect('/404')
+        return NextResponse.json({ status: 404, message: 'This page could not be found.' },{ status: 404 })
     }
 
-    return new NextResponse()
+    const token = await auth.getAccessToken()
+    if ( typeof videoMetadata.get( id ) !== 'object' ) {
+        try {
+            await fetch(`https://www.googleapis.com/drive/v3/files/${id}?fields=size,mimeType`, { method: 'GET', headers: { Authorization: `Bearer ${token}` } })
+                .then( r => r.json() )
+                .then( ( res: GoogleApisResponse ) => {
+                    if ( res.error ) {
+                        throw new Error('Not found')
+                    }
+                    videoMetadata.set( id, res )
+                })
+        } catch {
+            return NextResponse.json({ status: 404, message: `This page could not be found.` },{ status: 404 })
+        }
+    }
+
+    const { size, mimeType } = videoMetadata.get( id ) as Omit< GoogleApisResponse, 'error' >
+    const chunk_size = getParsedInt( searchParams.get('chunk') ) || CHUNK_SIZE
+    const start = parseInt( range.replace(/bytes=/,'').split('-')[0], 10 )
+    const end = Math.min( start + chunk_size - 1, parseInt( size, 10 ) )
+    const video = await fetch( `https://www.googleapis.com/drive/v3/files/${ id }?alt=media`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Range': `bytes=${start}-${end}`
+        }
+    })
+
+    const headers = new Headers({
+        "Content-Type": mimeType,
+        "Accept-Ranges": "bytes",
+        "Content-Range": `bytes ${start}-${end}/${size}`,
+        "Content-Length": ( end - start + 1 ).toString()
+    })
+
+    return new NextResponse( video.body, {
+        status: 206,
+        headers
+    })
+
 }
