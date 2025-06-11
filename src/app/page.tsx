730ca2f40a071b07d9d75a5b2bb1008c90eb47f5
TiwPhiraphan <tananphiraphan@outlook.com>
2025-06-11 05:36:51 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 export default function Home() {
     return (
         <>
-            <div className='pt-5 w-80 mx-auto max-w-full'>
-                <video className='w-full' src='/stream/video?watch=14HeZt5EQ2dxdBCNfa_kWHqesUEb0cKPK&bufferSize=2.4Mb' controls={ true }></video>
-            </div>
+            <section></section>
         </>
     )
 }
