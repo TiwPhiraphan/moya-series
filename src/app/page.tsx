549f45998482850f5806e31abb7313b66782d2e2
TiwPhiraphan <tananphiraphan@outlook.com>
2025-06-11 09:08:54 +0000

    edited

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,6 +79,10 @@ export default async function Home() {
                     <Link className='border-gray-300 rounded-2xl border px-3 py-1' href='/oAuth2/login'>เข้าสู่ระบบ</Link>
                 </nav>
                 <main className='py-6 w-full'>
+                    <div className='pb-10 flex justify-center'>
+                        <input className='px-6 py-2 flex-1 outline-none border rounded-s-full max-w-96 border-gray-400' placeholder='ค้นหาซีรีย์ที่คุณสนใจ' type='text'></input>
+                        <button className='ps-3 pe-5 border bg-black text-white border-black border-s-0 rounded-e-full'>ค้นหา</button>
+                    </div>
                     <Container metadata={ metadata } />
                 </main>
                 <footer className='py-5 text-center text-sm'>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ export default function Card({ data }:{ data: InterfaceMetadata }) {
     return (
         <figure>
             <Link className='group' href={ '/watch/' + data.id }>
-                <div className='overflow-hidden transition-all rounded-xl group-hover:rounded-none'>
+                <div className='overflow-hidden transition-all rounded-2xl group-hover:rounded'>
                     <Image className='w-full transition-all group-hover:scale-110' height={0} width={500} src={ `https://lh3.googleusercontent.com/d/${ data.thumbnail }=w500` } alt={ data.thumbnail }></Image>
                 </div>
                 <figcaption className='px-2 py-3'>
